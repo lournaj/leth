082ce8f330c606eaf0c8b27a946b48ddedd09d26
Greizgh <greizgh@ephax.org>
2016-01-04 23:22:53 +0100

    Add some relations

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
-from .models import Feed, Article
+from .models import Feed, Article, FeedSubscription, ReadingEntry
 
 
-class FeedAdmin(admin.ModelAdmin):
-    model = Feed
-
-
-class ArticleAdmin(admin.ModelAdmin):
-    model = Article
-
-admin.site.register(Feed, FeedAdmin)
-admin.site.register(Article, ArticleAdmin)
+admin.site.register(Feed)
+admin.site.register(Article)
+admin.site.register(FeedSubscription)
+admin.site.register(ReadingEntry)
--- src/core/models.py
+++ src/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Feed(models.Model):
@@ -23,7 +24,32 @@ class Article(models.Model):
     title = models.CharField(max_length=255, blank=True, null=True)
     content = models.TextField(blank=True, null=True)
     status = models.IntegerField(choices=Statuses, default=PENDING)
-    feed = models.ForeignKey(Feed, on_delete=models.SET_NULL, blank=True, null=True)
+    feed = models.ForeignKey(Feed, on_delete=models.SET_NULL,
+                             blank=True, null=True)
 
     def __str__(self):
         return self.url
+
+
+class FeedSubscription(models.Model):
+    """Association between a user and a feed"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} - {}".format(self.user, self.feed)
+
+
+class ReadingEntry(models.Model):
+    """Association between an article and a user"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Reading entry"
+        verbose_name_plural = "Reading entries"
+
+    def __str__(self):
+        return "{} - {}".format(self.user.username, self.article.url[:50])
