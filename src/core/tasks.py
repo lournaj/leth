7832165717b6e9deefeb7d12a488dfc924b90183
Greizgh <greizgh@ephax.org>
2016-01-12 19:18:58 +0100

    Fix fetch_feed

--- src/core/tasks.py
+++ src/core/tasks.py
@@ -1,17 +1,27 @@
+from django.utils import timezone
 from celery import shared_task
 from .models import Article, Feed
+import core.constants as cst
 import feedparser
 
 
 @shared_task
 def fetch_feed(feed_id):
+    feed = Feed.objects.get(pk=feed_id)
     try:
-        feed = Feed.objects.get(pk=feed_id)
         data = feedparser.parse(feed.url)
-        for post in data['items']:
-            print(post)
-            article = Article(url=post['link'], title=post['title'], content=post['description'], feed=feed)
+        for post in data.entries:
+            article = Article(url=post.link, title=post.title,
+                              content=post.summary, feed=feed)
             article.save()
     except Exception as e:
         print(e)
-        pass
+        feed.status = cst.ERROR_STATUS
+        feed.save()
+    else:
+        if feed.status != cst.READY_STATUS:
+            feed.status = cst.READY_STATUS
+        if not feed.name:
+            feed.name = data.feed.title
+        feed.last_fetch = timezone.now()
+        feed.save()
